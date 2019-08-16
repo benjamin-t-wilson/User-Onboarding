0c9640d419c1f401d48827b124fcb3139d745904
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-08-15 20:00:48 -0700

    completed MVP for project. Started styling

---
+++ src/components/UserCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function UserCard(props) {
+    return (
+        <>
+        <h1>Users</h1>
+        <div className="userCard">
+            <h1 className="userName">{props.user.name}</h1>
+            <p className="userEmail">Email: {props.user.email}</p>
+            <p className="userDate">Joined: {props.user.date}</p>
+        </div>
+        </>
+    )
+
+}
+
+export default UserCard;
